c44b16c602ec7504d948cc338874a70d0c60d95d
Supstoov81 <mattbib@gmail.com>
2024-07-12 17:27:48 +0200

    Alogorithmie

--- src/algorithmie/AffichageInverse.java
+++ src/algorithmie/AffichageInverse.java
@@ -4,12 +4,17 @@ public class AffichageInverse {
 	
 	public static void main(String[] args) {
 		//Déclaration du tableau
+		System.out.println("Affichage dans l'ordre normal :");
 		int [] array = {1,15,-3,0,8,7,4,-2,28,7,-1,17,2,3,0,14,-4};
 		//Afficher l'ensemble des éléments des tableau
 		for(int i=0; i<array.length;i++) {
 			System.out.println(array[i]);
 		}
+		
+		// Ajouter une ligne vide entre les deux blocs d'exécution
+        System.out.println("\n");
 		////Afficher l'ensemble des éléments des tableau inversé
+        System.out.println("Affichage dans l'ordre inversé :");
 		 for (int i = array.length - 1; i >= 0; i--) {
 	            System.out.println(array[i]);
 	        }
--- src/algorithmie/AffichageSuite.java
+++ src/algorithmie/AffichageSuite.java
@@ -3,17 +3,27 @@ package algorithmie;
 public class AffichageSuite {
 	public static void main(String[] args) {
 		//Affichage de tous les nombres de 1 à 10
+		System.out.println("Affichage de tous les nombres de 1 à 10");
 		for(int i = 1;i<=10;i++) {
 			System.out.println(i);
 			
 		}
+		
+		// Ajouter une ligne vide entre les deux blocs d'exécution
+        System.out.println("\n");
+		
 		//Affichage de tous les nombres paires entre 0 et 20
+		System.out.println("Affichage de tous les nombres paires entre 0 et 20:");
 		for(int i = 0;i<=20;i++) {
 			if(i%2==0)
 			System.out.println(i);
 		}
 		
+		// Ajouter une ligne vide entre les deux blocs d'exécution
+        System.out.println("\n");
+		
 		//Affichage de tous les nombres impaires entre 0 et 9
+		System.out.println("Affichage de tous les nombres impaires entre 0 et 9:");
 		for(int i = 0;i<=9;i++) {
 			if(i%2!=0)
 			System.out.println(i);
