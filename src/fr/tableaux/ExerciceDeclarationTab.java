0c66ec00c3d6939ac21095675928558a057a646c
Supstoov81 <mattbib@gmail.com>
2024-07-12 15:50:43 +0200

    test

---
+++ src/fr/tableaux/ExerciceDeclarationTab.java
@@ -0,0 +1,17 @@
+package fr.tableaux;
+
+
+
+public class ExerciceDeclarationTab {
+	public static void main(String[] args) {
+		
+		//Déclaration tableau
+		int[] tab = {8,-7,12,1,-2,14,17,9};
+		
+		System.out.println(tab[0]);
+		System.out.println(tab.length);
+		System.out.println(tab[7]);
+		
+	}
+
+}
